ab43bdb795c44026007f13f677f40263bd5c39d2
Evgeny Yushkevich <evgeny_yushkevich@mail.ru>
2017-06-08 18:41:35 +0300

    new command list

--- src/main/java/by/shiler/neurox/command/AvailableOperations.java
+++ src/main/java/by/shiler/neurox/command/AvailableOperations.java
@@ -6,19 +6,12 @@ package by.shiler.neurox.command;
 public class AvailableOperations {
 
     public static final String MENU_COMMAND = "menu";
-    public static final String VIEW_COMMAND = "view";
-    public static final String REPORT_COMMAND = "report";
-    public static final String RENAME_COMMAND = "rename";
-    public static final String EXIT_COMMAND = "exit";
+    public static final String STORE_COMMAND = "store";
+    public static final String TRAIN_COMMAND = "train";
+    public static final String PREDICT_COMMAND = "predict";
 
+    public static final String STORE_PARAM = "games";
     public static final String BOOKS_PARAM = "books";
-    public static final String EMPLOYEES_LONG_PARAM = "employees";
-    public static final String EMPLOYEES_SHORT_PARAM = "emps";
-    public static final String LESS_OR_EQ_THAN_2_PARAM = "<=2";
-    public static final String MORE_THAN_1_PARAM = ">1";
-    public static final String MASK_PARAM = "m";
-    public static final String TITLE_PARAM = "t";
-
 
     public static final String INVALID_COMMAND_MESSAGE = "Failed: invalid command";
     public static final String INVALID_PARAMETER_LIST_MESSAGE = "Failed: invalid parameter list";
